df5309362b3efac2d3cd0c498bab1bdb8c694b8c
Tarek Belkahia <tarek.belkahia@gmail.com>
2018-01-20 11:04:06 +0100

    Add layout

--- src/main/kotlin/firebasepush.kt
+++ src/main/kotlin/firebasepush.kt
@@ -1,5 +1,52 @@
 import tornadofx.*
 
 class MainView : View("Firebase Push") {
-    override val root = form {}
+
+    override val root = form {
+        fieldset("Config") {
+            field("Server Key") {
+                textfield()
+            }
+            field("Tokens") {
+                textarea {
+                    prefRowCount = 4
+                    isWrapText = false
+                }
+            }
+        }
+        fieldset("Payload") {
+            hbox {
+                field {
+                    checkbox("Notification")
+                }
+                vbox {
+                    field("Title") {
+                        textfield()
+                    }
+                    field("Body") {
+                        textfield()
+                    }
+                }
+            }
+            hbox {
+                field {
+                    checkbox("Data")
+                }
+                vbox {
+                    hbox {
+                        field("Key") {
+                            textfield()
+                        }
+                        field("Value") {
+                            textfield()
+                        }
+                        button("+")
+                    }
+                }
+            }
+        }
+        button("Send")
+    }
 }
+
+class FirebasePushApp : App(MainView::class)
